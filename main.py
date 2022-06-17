5bb803840ec209ee1d1d3ed65a23fe9ca0ec5248
Alfredo Bravo Cuero <yo@alfredobravocuero.co>
2022-06-16 20:21:09 -0500

    2: Template, condicionales y ciclos

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-from flask import Flask, make_response, redirect, request
+from multiprocessing import context
+from flask import Flask, make_response, redirect, render_template, request
 
 from flask import Flask
 
 app = Flask(__name__)
 
+todos = ['Learn Python', 'Learn Flask', 'Learn React']
+
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
+
     response = make_response(redirect('/hello'))
     response.set_cookie('user_ip', user_ip)
+
     return response
 
 
-@ app.route('/hello')
-def hello_world():
+@app.route('/hello')
+def hello():
     user_ip = request.cookies.get('user_ip')
-    return 'Hello World! tu IP es: {}'.format(user_ip)
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+    }
+    return render_template('hello.html', **context)
